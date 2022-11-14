19b638397991bc5849762d5ab45533a8444e7d50
passwordtt <tarik.tlb@hotmail.com>
2022-11-14 11:41:40 +0100

    C1

---
+++ php/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "dierenarts";
+
+    $conn = new mysqli($host,$username,$password,$db) or die($conn -> connect_error);
+
+?>
